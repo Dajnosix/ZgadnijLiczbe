469e29f54a016cdb7272b798b7c5401e5d468abd
Dawid Jaworski <minerpl03@gmail.com>
2020-09-17 17:31:17 +0200

    Aplikacja teraz nie wywala kiedy zamiast liczby dasz symbol/nic

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ from random import randint
 random = randint(1,25)
 answer = -1
 times = 0
+
 print("Mini-gra Zgadnij Liczbę by Dajnosix" )
 print("Gra inspirowana kursami na Youtube.")
-print("Podaj w przedziale od 1 - 25!")
+print("Podaj liczbę w przedziale od: 1-25!")
 
 while answer != random:
     times += 1
-    answer = int(input("Podaj liczbę: "))
-    if answer > random:
-        print("Wylosowana liczba jest mniejsza od tej napisanej przez ciebie!")
-    elif answer < random:
-        print("Wylosowana liczba jest większa od tej wpisanej przez ciebie!")
+    try:
+        answer = int(input("Podaj liczbę: "))
+        if answer > random:
+            print("Wylosowana liczba jest mniejsza od tej napisanej przez ciebie!")
+        elif answer < random:
+            print("Wylosowana liczba jest większa od tej wpisanej przez ciebie!")
+    except ValueError:
+        print("Nie podałeś liczby, bądź jest ona nie prawidłowa")
+        answer = -1
+
 print(f"Gratulacje! Ogadłeś prawidłową liczbę za {times} razem.")
